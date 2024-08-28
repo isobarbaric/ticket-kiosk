b4c597ff960c245ebe3c01637538b3c02deba6bf
Krish Shah <shahkrish2016@gmail.com>
2024-08-27 23:11:37 -0400

    setup basic login page and changed main app to be `App` instead of `KanbanBoard`

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { ref } from 'vue'
+
+const username = ref('');
+const password = ref('');
+const isLoggedIn = ref(false);
+
+// TODO: redirect to boards page after login (new component)
+// create a useBoardsStore()
+function processLogin() {
+  console.log('Logging in as', username.value, 'with password', password.value);
+  isLoggedIn.value = true;
+  // store.fetchCards();
+}
+</script>
+
+<!-- add dual slider -->
+<template>
+  <div class="login-panel">
+    <div class="username">
+      <div>Username</div>
+      <input type="text" v-model="username" size="40" />
+    </div>
+    <div class="password">
+      <div>Password</div>
+      <input type="password" v-model="password" size="40" />
+    </div>
+    <button v-on:click="processLogin">Login</button>
+  </div>
+</template>
+
+<style scoped>
+.login-panel {
+  position: absolute;
+  left: 50%;
+  top: 30%;
+  transform: translate(-50%, -50%);
+  border: 1px solid black;
+}
+
+.username, .password {
+  font-size: 20px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  padding: 0.5em;
+}
+
+input {
+  border: 2px solid black;
+  border-radius: 5px;
+  font-size: 15px;
+}
+
+button {
+  margin-top: 10px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import KanbanBoard from './KanbanBoard.vue'
+import App from './App.vue'
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 const pinia = createPinia()
-const app = createApp(KanbanBoard)
+const app = createApp(App)
 
 library.add(faCheck, faXmark);
 app.component("font-awesome-icon", FontAwesomeIcon);
